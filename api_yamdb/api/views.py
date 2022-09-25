f32ba5b9082287de5327a11807289b2037e7cd43
Ivan Shamaiski <shamaiskii.ivan@gmail.com>
2022-09-25 21:46:21 +0500

    Fixed test with flake8 and pytest

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from api_yamdb.settings import EMAIL_ADMIN
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
@@ -12,6 +11,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Comment, Genre, Review, Title, User
+
+from api_yamdb.settings import EMAIL_ADMIN
 
 from .filters import TitleFilter
 from .mixins import CreateListDestroyViewSet
@@ -51,7 +52,7 @@ class UserViewSet(viewsets.ModelViewSet):
                 serializer.data,
                 status=status.HTTP_200_OK
             )
-        if request.method == 'PATCH':
+        else:
             serializer = UserSerializerOrReadOnly(
                 request.user,
                 data=request.data,
